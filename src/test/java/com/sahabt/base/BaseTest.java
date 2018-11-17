36bc65a64c2277c5ccc48210323f9f7d22e7b153
info@t-con.co.uk <tomcat8@ip-172-31-44-76.eu-west-1.compute.internal>
2018-11-17 22:42:04 +0000

    updated

--- src/test/java/com/sahabt/base/BaseTest.java
+++ src/test/java/com/sahabt/base/BaseTest.java
@@ -3,11 +3,17 @@ package com.sahabt.base;
 import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.JavascriptExecutor;
+import org.openqa.selenium.WebElement;
 import org.openqa.selenium.remote.DesiredCapabilities;
 import org.openqa.selenium.remote.RemoteWebDriver;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 
 import java.net.MalformedURLException;
 import java.net.URL;
+import java.util.List;
 
 public class BaseTest {
 
@@ -24,14 +30,31 @@ public class BaseTest {
 			e.printStackTrace();
 		}
 	}
-	
-	@Test
-    public void testCase(){
-        navigate("http://www.hepsiburada.com/");
+
+    @Test
+    public void searchTest(){
+        navigate("http://www.google.com");
+
+		JavascriptExecutor jse = (JavascriptExecutor)driver;
+        jse.executeScript("window.scrollBy(0,250)", "");
+
+        List<WebElement> elements = driver.findElements(By.tagName("a"));
+    	for (WebElement webElement : elements) {
+			WebDriverWait webDriverWait = new WebDriverWait(driver, 2);
+			WebElement waitElement = webDriverWait.until(ExpectedConditions.elementToBeClickable(webElement));
+			if(waitElement != null) {
+				try {
+					webElement.click();
+					break;
+				} catch (Exception e) {
+					continue;
+				}
+			}
+    	}
     }
 
     public void navigate(String url) {
-        driver.get(url);
+        driver.navigate().to(url);
     }
 
 	@After
